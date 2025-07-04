3a1230eb4de7e2530f8cf116cd0c75cf1767eb45
Cristian Cuellar <ccuellar260@gmail.com>
2025-07-04 15:23:31 -0400

    brexe

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,39 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-
-
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/saludo', function () {
-        return view('saludos');
-    })->name('saludo');
 
-    Route::get('/producto', function () {
-        return view('productos');
-    })->name('producto');
+
+
+Route::get('/productos', function () {
+    return Inertia::render('Producto');
+})->name('producto');
+
+Route::get('/saludos', function () {
+    return Inertia::render('Saludo');
+})->name('saludo');
+
+
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
